48311056cdafbd11f9241e12ab77ef2c30fd2c07
Dustin <dmkelly100@gmail.com>
2020-07-29 15:40:53 -0400

    tried to add moduels but need to remove namespace

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import tips from './modules/tipData.js';
 // nameSpace
 const game = {};
 
@@ -131,7 +132,6 @@ game.checkAnswer = () => {
 		generateQs();
 		scoreUp();
 		game.displayRightWrong(right);
-
 	} else {
 		game.displayRightWrong(wrong);
 		scoreDown();
@@ -231,15 +231,16 @@ game.handleCloseScoreBox = () => {
 	game.getTip();
 };
 
+// show / hide function
 game.displayRightWrong = (url) => {
 	const img = document.querySelector('.correctWrong');
 	img.src = url;
 	img.classList.add('show');
-	 setTimeout(flash, 500);
+	setTimeout(flash, 500);
 	function flash() {
 		img.classList.remove('show');
 	}
-}
+};
 
 game.newGame = () => {
 	game.reset();
@@ -251,6 +252,9 @@ game.newGame = () => {
 game.reset = () => {
 	game.keywordCategory = '';
 	game.totalScore = 0;
+	game.gameScore.innerHTML = 0;
+	game.countDown.innerHTML = 45;
+
 	game.time = 45;
 };
 
@@ -278,7 +282,7 @@ game.firebaseConfig = {
 firebase.initializeApp(game.firebaseConfig);
 
 game.getTip = () => {
-	const storeTip = game.getRandom(game.tips);
+	const storeTip = game.getRandom(tips);
 	const insertTip = document.querySelector('.tipChange');
 	insertTip.innerHTML = `<h2>Waste Tip:</h2>
             <h3>${storeTip.tip}</h3>
@@ -286,48 +290,3 @@ game.getTip = () => {
 			<p class="tipText">${storeTip.des}</p>`;
 	console.log(insertTip);
 };
-
-// Tips Data
-game.tips = [
-	{
-		tip: 'Paper coffee cups',
-		img: './imgs/trashTip.png',
-		des: `Place Tim Hortons and other paper hot beverage cups in the garbage bin. 
-The plastic lining and dyes on these paper cups make them difficult to recycle at this time. Non-black plastic lids and paper sleeves should be removed and placed in the Blue Bin.`,
-	},
-	{
-		tip: 'Clothing and textiles',
-		img: './imgs/trashTip.png',
-		des: `Old clothes, shoes, blankets, and curtains don’t belong in the Blue Bin. They can get caught in sorting machines, damage equipment and cause workplace injuries at the recycling facility. If your items can’t be donated, put them in the garbage.`,
-	},
-	{
-		tip: 'Black plastic',
-		img: './imgs/trashTip.png',
-		des: `Black plastic of any kind, such as take-out containers and black garbage bags aren’t recyclable. Black plastic cannot be sorted mechanically at the recycling facility and there is no market for the volume of black plastic the city generates. Please dispose of all black plastic in the garbage.`,
-	},
-	{
-		tip: 'Clean containers only',
-		img: './imgs/recycleTip.png',
-		des: ` Remove food, liquids or other contents and rinse clean before recycling. When you don’t, the residue from items like jars and take-out containers get soaked up by paper and can ruin large batches of otherwise good recyclables.`,
-	},
-	{
-		tip: 'Compact fluorescent bulbs, oil, batteries, etc.',
-		img: './imgs/hazardousWasteTip.png',
-		des: `Household hazardous waste items must never be put in recycling or garbage. Take these items to a drop-off depot or call the Toxic Taxi for free pick-up. Not sure if you have hazardous waste? Use the Waste Wizard site or app to check! `,
-	},
-	{
-		tip: 'Corrugated cardboard',
-		img: './imgs/recycleTip.png',
-		des: `Clean, unwaxed, flattened corrugated cardboard goes in the recycling bin. Pizza boxes must be empty; remove over-wrap from water/soft drink cases, recycle separately.`,
-	},
-	{
-		tip: 'Food and organic waste',
-		img: './imgs/greenTip.png',
-		des: `Food scraps like apple cores, eggshells or expired leftovers belong in your Green Bin. When you mistakenly toss food scraps in your Blue Bin, food residue and particles get soaked up by paper and can ruin large batches of otherwise good recyclables.`,
-	},
-	{
-		tip: 'Chains, hoses and electrical cords',
-		img: './imgs/trashTip.png',
-		des: `These do not go in your Blue Bin. They can get tangled in sorting machines, damage equipment and cause workplace injuries at the recycling facility. See if your local electronics store has a recycling program. Throw unwanted cords, hoses and cables in your garbage.`,
-	},
-];
